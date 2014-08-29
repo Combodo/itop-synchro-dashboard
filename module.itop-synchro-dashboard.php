b3b81e82fee2631f3b0e8ac6786e5197bfd173bf
Denis Flaven <denis.flaven@combodo.com>
2014-08-29 13:48:28 +0000

    First version of the itop-synchro-dashboard module
    SVN:trunk[3835]

--- module.itop-synchro-dashboard.php
+++ module.itop-synchro-dashboard.php
@@ -5,7 +5,7 @@
 
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'itop-synchro-dashboard/',
+	'itop-synchro-dashboard/1.0.0',
 	array(
 		// Identification
 		//
