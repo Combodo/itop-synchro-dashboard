c9636a91a7c4175eecd839ba0c6232bd0f94dedc
Eric <eric.espie@combodo.com>
2019-12-11 14:00:12 +0100

    N°2595 - Reorganize/rename admin. console menus

--- main.itop-synchro-dashboard.php
+++ main.itop-synchro-dashboard.php
@@ -6,8 +6,8 @@ class ItopSynchroDashboardMenus extends ModuleHandlerAPI
 		// Add the admin menus
 		if (UserRights::IsAdministrator())
 		{
-			$oAdminMenu = new MenuGroup('AdminTools', 80 /* fRank */, 'SynchroDataSource', UR_ACTION_MODIFY, UR_ALLOWED_YES);
-			new WebPageMenuNode('DataSourcesDashboard', utils::GetAbsoluteUrlAppRoot().'pages/exec.php?exec_module=itop-synchro-dashboard&exec_page=dashboard.php', $oAdminMenu->GetIndex(), 13 /* fRank */);
+			$oAdminMenu = new MenuGroup('Configuration', 90 /* fRank */, 'SynchroDataSource', UR_ACTION_MODIFY, UR_ALLOWED_YES);
+			new WebPageMenuNode('DataSourcesDashboard', utils::GetAbsoluteUrlAppRoot().'pages/exec.php?exec_module=itop-synchro-dashboard&exec_page=dashboard.php', $oAdminMenu->GetIndex(), 25 /* fRank */);
 		}
 	}
 }
