fc55d705d2fcfbc0988dffc6005214b5231f0292
Molkobain <lajarige.guillaume@free.fr>
2020-02-07 17:38:24 +0100

    N°2595 - Reorganize admin. console menus: Move to "Configuration tools" in iTop 2.7+

--- main.itop-synchro-dashboard.php
+++ main.itop-synchro-dashboard.php
@@ -1,12 +1,20 @@
 <?php
 class ItopSynchroDashboardMenus extends ModuleHandlerAPI
 {
+	/**
+	 * @inheritDocc
+	 * @throws \Exception
+	 */
 	public static function OnMenuCreation()
 	{
 		// Add the admin menus
 		if (UserRights::IsAdministrator())
 		{
-			$oAdminMenu = new MenuGroup('Configuration', 90 /* fRank */, 'SynchroDataSource', UR_ACTION_MODIFY, UR_ALLOWED_YES);
+			// From iTop 2.7, the "ConfigurationTools" menu group exists
+			// Before, only "AdminTools" was available for that kind of entry
+			$sParentMenuId = ApplicationMenu::GetMenuIndexById('ConfigurationTools') > -1 ? 'ConfigurationTools' : 'AdminTools';
+
+			$oAdminMenu = new MenuGroup($sParentMenuId, 90 /* fRank */, 'SynchroDataSource', UR_ACTION_MODIFY, UR_ALLOWED_YES);
 			new WebPageMenuNode('DataSourcesDashboard', utils::GetAbsoluteUrlAppRoot().'pages/exec.php?exec_module=itop-synchro-dashboard&exec_page=dashboard.php', $oAdminMenu->GetIndex(), 25 /* fRank */);
 		}
 	}
