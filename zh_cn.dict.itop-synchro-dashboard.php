8a7b12bee87ed54e37ed9046ac2f1190f88ae373
Dejin Bie <bdejin@hotmail.com>
2022-05-19 13:43:26 +0800

    :globe_with_meridians: Update Chinese translations

--- zh_cn.dict.itop-synchro-dashboard.php
+++ zh_cn.dict.itop-synchro-dashboard.php
@@ -20,29 +20,30 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with iTop. If not, see <http://www.gnu.org/licenses/>
  */
+
 Dict::Add('ZH CN', 'Chinese', '简体中文', array(
 	// Dictionary entries go here
-	'UI:SynchroDashboard:Title' => 'Data Synchro Dashboard~~',
-	'UI:SynchroDashboard:StartDate' => 'Start Date~~',
-	'UI:SynchroDashboard:StartDate+' => 'Start date of the latest execution~~',
-	'UI:SynchroDashboard:Duration' => 'Duration~~',
-	'UI:SynchroDashboard:Duration+' => 'Duration of the latest execution~~',
-	'UI:SynchroDashboard:NbOfReplicas' => 'Number of replicas~~',
-	'UI:SynchroDashboard:NbOfReplicas+' => 'Total number of replicas for this synchro data source~~',
-	'UI:SynchroDashboard:NbOfErrors' => 'Errors~~',
-	'UI:SynchroDashboard:NbOfErrors+' => 'Number of errors encountered during the latest run~~',
-	'UI:SynchroDashboard:NbOfWarnings' => 'Warnings~~',
-	'UI:SynchroDashboard:NbOfWarnings+' => 'Number of warnings encountered during the latest run~~',
-	'UI:SynchroDashboard:PeakMemoryUsage' => 'Peak Memory Usage~~',
-	'UI:SynchroDashboard:PeakMemoryUsage+' => 'Maximum amount of memory used during the synchronization~~',
+	'UI:SynchroDashboard:Title' => '数据同步仪表盘',
+	'UI:SynchroDashboard:StartDate' => '开始日期',
+	'UI:SynchroDashboard:StartDate+' => '最近一次运行的开始日期',
+	'UI:SynchroDashboard:Duration' => '持续时间',
+	'UI:SynchroDashboard:Duration+' => '最近一次运行的持续时间',
+	'UI:SynchroDashboard:NbOfReplicas' => '复制数量',
+	'UI:SynchroDashboard:NbOfReplicas+' => '此同步数据源的复制总数量',
+	'UI:SynchroDashboard:NbOfErrors' => '错误',
+	'UI:SynchroDashboard:NbOfErrors+' => '最近一次运行时的错误数',
+	'UI:SynchroDashboard:NbOfWarnings' => '告警',
+	'UI:SynchroDashboard:NbOfWarnings+' => '最近一次运行时的告警数',
+	'UI:SynchroDashboard:PeakMemoryUsage' => '内存使用峰值',
+	'UI:SynchroDashboard:PeakMemoryUsage+' => '同步时最大使用的内存数',
 	// Tab labels
-	'UI:SynchroDashboard:Overview:LatestRun' => 'Latest Run~~',
+	'UI:SynchroDashboard:Overview:LatestRun' => '最近运行',
 	// Shorter labels for the badges at the top of the overview
-	'UI:SynchroDashboard:Overview:DataSources' => 'Data Sources~~',
-	'UI:SynchroDashboard:Overview:RunTime' => 'Run Time~~',
-	'UI:SynchroDashboard:Overview:Replicas' => 'Replicas~~',
-	'UI:SynchroDashboard:Overview:Errors' => 'Errors~~',
-	'UI:SynchroDashboard:Overview:Warnings' => 'Warnings~~',
-	'UI:SynchroDashboard:Overview:PeakMemory' => 'Peak Memory~~',
-	'Menu:DataSourcesDashboard' => 'Synchronization Dashboard~~',
+	'UI:SynchroDashboard:Overview:DataSources' => '数据源',
+	'UI:SynchroDashboard:Overview:RunTime' => '运行时长',
+	'UI:SynchroDashboard:Overview:Replicas' => '复制',
+	'UI:SynchroDashboard:Overview:Errors' => '错误',
+	'UI:SynchroDashboard:Overview:Warnings' => '告警',
+	'UI:SynchroDashboard:Overview:PeakMemory' => '内存峰值',
+	'Menu:DataSourcesDashboard' => '同步仪表盘',
 ));
