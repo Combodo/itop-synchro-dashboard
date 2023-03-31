df98c93b5db427840d5451321995151bf89c6322
odain <olivier.dain@combodo.com>
2023-03-31 10:58:10 +0200

    PR feedbacks: restore empty model.itop-synchro-dashboard.php

--- model.itop-synchro-dashboard.php
+++ model.itop-synchro-dashboard.php
@@ -1,1 +1,4 @@
 <?php
+
+//left empty on purpose
+//without this file for iTop 3.0 and older could crash if someone moves the menu defined in datamodel.itop-synchro-dashboard.xml to another module / parent menu (typically through the ITSM Designer)
