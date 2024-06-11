8dde0a762786691c2000b15f169fad90054c60e9
Timothee <timothee.callet@combodo.com>
2024-06-11 16:59:02 +0200

    Automated dictionaries synchronization with english reference

--- zh_cn.dict.itop-synchro-dashboard.php
+++ zh_cn.dict.itop-synchro-dashboard.php
@@ -2,48 +2,33 @@
 /**
  * Localized data
  *
- * @copyright Copyright (C) 2010-2018 Combodo SARL
- * @license	http://opensource.org/licenses/AGPL-3.0
- *
- * This file is part of iTop.
- *
- * iTop is free software; you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * iTop is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with iTop. If not, see <http://www.gnu.org/licenses/>
+ * @copyright Copyright (C) 2010-2024 Combodo SAS
+ * @license    https://opensource.org/licenses/AGPL-3.0
+ * 
  */
-
-Dict::Add('ZH CN', 'Chinese', '简体中文', array(
-	// Dictionary entries go here
-	'UI:SynchroDashboard:Title' => '数据同步仪表盘',
-	'UI:SynchroDashboard:StartDate' => '开始日期',
-	'UI:SynchroDashboard:StartDate+' => '最近一次运行的开始日期',
+/**
+ *
+ */
+Dict::Add('ZH CN', 'Chinese', '简体中文', [
+	'Menu:DataSourcesDashboard' => '同步仪表盘',
 	'UI:SynchroDashboard:Duration' => '持续时间',
 	'UI:SynchroDashboard:Duration+' => '最近一次运行的持续时间',
-	'UI:SynchroDashboard:NbOfReplicas' => '复制数量',
-	'UI:SynchroDashboard:NbOfReplicas+' => '此同步数据源的复制总数量',
 	'UI:SynchroDashboard:NbOfErrors' => '错误',
 	'UI:SynchroDashboard:NbOfErrors+' => '最近一次运行时的错误数',
+	'UI:SynchroDashboard:NbOfReplicas' => '复制数量',
+	'UI:SynchroDashboard:NbOfReplicas+' => '此同步数据源的复制总数量',
 	'UI:SynchroDashboard:NbOfWarnings' => '告警',
 	'UI:SynchroDashboard:NbOfWarnings+' => '最近一次运行时的告警数',
+	'UI:SynchroDashboard:Overview:DataSources' => '数据源',
+	'UI:SynchroDashboard:Overview:Errors' => '错误',
+	'UI:SynchroDashboard:Overview:LatestRun' => '最近运行',
+	'UI:SynchroDashboard:Overview:PeakMemory' => '内存峰值',
+	'UI:SynchroDashboard:Overview:Replicas' => '复制',
+	'UI:SynchroDashboard:Overview:RunTime' => '运行时长',
+	'UI:SynchroDashboard:Overview:Warnings' => '告警',
 	'UI:SynchroDashboard:PeakMemoryUsage' => '内存使用峰值',
 	'UI:SynchroDashboard:PeakMemoryUsage+' => '同步时最大使用的内存数',
-	// Tab labels
-	'UI:SynchroDashboard:Overview:LatestRun' => '最近运行',
-	// Shorter labels for the badges at the top of the overview
-	'UI:SynchroDashboard:Overview:DataSources' => '数据源',
-	'UI:SynchroDashboard:Overview:RunTime' => '运行时长',
-	'UI:SynchroDashboard:Overview:Replicas' => '复制',
-	'UI:SynchroDashboard:Overview:Errors' => '错误',
-	'UI:SynchroDashboard:Overview:Warnings' => '告警',
-	'UI:SynchroDashboard:Overview:PeakMemory' => '内存峰值',
-	'Menu:DataSourcesDashboard' => '同步仪表盘',
-));
+	'UI:SynchroDashboard:StartDate' => '开始日期',
+	'UI:SynchroDashboard:StartDate+' => '最近一次运行的开始日期',
+	'UI:SynchroDashboard:Title' => '数据同步仪表盘',
+]);
